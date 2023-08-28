bba1965c1647124edec8c38f90b3036145c03463
felipe pedro <felipepedro.senai564@gmail.com>
2023-08-28 09:29:42 -0300

    função verificação url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 function verificarInputs() {
+
     let tutor = document.getElementById('tutor').value
     let petName = document.getElementById('petName').value
     let especie = document.getElementById('especie').value
@@ -13,15 +14,29 @@ function verificarInputs() {
 
     if (tutor == "" || petName == "" || especie == "" || imgLink == "" || birthdate == "") {
         console.log("inputs vazios");
-        evieMsg("preencha todos os campos", "erro")
+        envieMsg("preencha todos os campos", "erro");
+        return true;
+
+    } else if (!isURLValida(imgLink)) {
+        evieMsg("URL da imagem inválida!", "erro");
+
     } else {
         console.log("inouts preenchidos")
-        evieMsg("cadastro concluido com sucesso", "sucesso")
+        envieMsg("cadastro concluido com sucesso", "sucesso");
+        return false;
+    }
+}
+function isURLValida(url) {
+    if (url.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+        return true;
+    } else {
+        return false;
     }
 }
 
+
 function envieMsg(msg, tipoMsg) {
-    let msgDiv = document.getElementById('msg').value
+    let msgDiv = document.getElementById('msg');
     let msgEnviar = `
     <p class = "${tipoMsg}">${msg}</p>`;
     msgDiv.innerHTML = msgEnviar;
@@ -43,17 +58,37 @@ class Pet {
 const petTest = new Pet("felipe", "alvim", "shitzu", "link",);
 console.log(petTest);
 
+function comporPet() {
+
+    let tutor = document.getElementById('tutor').value
+    let petName = document.getElementById('petName').value
+    let especie = document.getElementById('especie').value
+    let imgLink = document.getElementById('imgLink').value
+    let birthdate = document.getElementById('birthdate').value
+
+    const pet = new Pet(tutor, petName, especie, imgLink, birthdate);
+
+
+    bibliotecaPets.adicionarPet(pet);
+    console.log(bibliotecaPets)
+
+}
+
 class listaPets {
     constructor() {
         this.listaPetsArray = [];
     }
 
     adicionarPet(param) {
-        if (verificarInputs() == false)
+        console.log("Método");
+        if (verificarInputs() == false) {
+            console.log("Dentro");
+
+
             this.listaPetsArray.push(param);
 
             limparInputs();
-
+        }
     }
 }
 const bibliotecaPets = new listaPets();
@@ -61,17 +96,17 @@ const bibliotecaPets = new listaPets();
 function limparInputs() {
     tutor = document.getElementById('tutor').value = "";
     petName = document.getElementById('petName').value = "";
-    especie = document.getElementById('especie').value = ""; 
-    imgLink = document.getElementById('imgLink').value = ""; 
-    birthdate = document.getElementById('birthdate').value = ""; 
+    especie = document.getElementById('especie').value = "";
+    imgLink = document.getElementById('imgLink').value = "";
+    birthdate = document.getElementById('birthdate').value = "";
 }
 
-function renderizarConteudo(content){
+function renderizarConteudo() {
 
-content = "";
+    let content = "";
 
-bibliotecaPets.listaPetsArray.forEach(pet => {
-    content += `
+    bibliotecaPets.listaPetsArray.forEach(pet => {
+        content += `
     <div class="petCadastro">
             <p>tutor:${pet.tutor}</p>
             <p>nome do pet:${pet.petName}</p>
@@ -81,6 +116,6 @@ bibliotecaPets.listaPetsArray.forEach(pet => {
         </div>
     `;
 
+    });
     document.getElementById("petList").innerHTML = content
-});
 }
